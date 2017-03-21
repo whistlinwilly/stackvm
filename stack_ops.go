7ba9a22d015eac398725e9584fd7841c04392862
Joshua T Corbin <jcorbin@wunjo.org>
2017-03-21 08:27:02 -0700

    Fix swap implementation

--- stack_ops.go
+++ stack_ops.go
@@ -29,19 +29,26 @@ func (arg _dup) run(m *Mach) error {
 }
 
 func (arg _swap) run(m *Mach) error {
-	addr1, err := m.pAddr(int32(arg))
+	addr1, err := m.pAddr(0)
 	if err != nil {
 		return err
 	}
-	addr2, err := m.pAddr(int32(arg + 1))
+	addr2, err := m.pAddr(int32(arg))
 	if err != nil {
 		return err
 	}
-	// TODO: better with gather -> scatter
-	i1, _, pg1 := m.pageFor(addr1)
-	i2, _, pg2 := m.pageFor(addr2)
-	pg1.d[i1], pg2.d[i2] = pg2.d[i2], pg1.d[i1]
-	return nil
+	val1, err := m.fetch(addr1)
+	if err != nil {
+		return err
+	}
+	val2, err := m.fetch(addr2)
+	if err != nil {
+		return err
+	}
+	if err := m.store(addr1, val2); err != nil {
+		return err
+	}
+	return m.store(addr2, val1)
 }
 
 func push(arg uint32, have bool) op {
