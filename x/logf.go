b078ef14f25fa7160efe5bca8fbf190e864ae50f
Joshua T Corbin <jcorbin@wunjo.org>
2017-03-25 15:24:08 -0700

    Improve logfTracer output

--- x/logf.go
+++ x/logf.go
@@ -2,6 +2,8 @@ package xstackvm
 
 import "github.com/jcorbin/stackvm"
 
+const opWidth = 15
+
 // LogfTracer implements a simple stackvm.Tracer that prints to a
 // formatted log function.
 type LogfTracer func(string, ...interface{})
@@ -17,17 +19,22 @@ func (logf LogfTracer) End(m *stackvm.Mach) {
 }
 
 // Before logs an operation about to be executed.
-func (logf LogfTracer) Before(m *stackvm.Mach, _ uint32, op stackvm.Op) {
-	logf(">>> % 10v in %v", op, m)
+func (logf LogfTracer) Before(m *stackvm.Mach, ip uint32, op stackvm.Op) {
+	ps, cs, err := m.Stacks()
+	if err != nil {
+		logf(">>> % *v @0x%04x 0x%04x:0x%04x 0x%04x:0x%04x ERROR %v", opWidth, op, ip, m.PBP(), m.PSP(), m.CSP(), m.CBP(), err)
+	} else {
+		logf(">>> % *v @0x%04x %v :0x%04x 0x%04x: %v", opWidth, op, ip, ps, m.PSP(), m.CSP(), cs)
+	}
 }
 
 // After logs the result of executing an operation.
 func (logf LogfTracer) After(m *stackvm.Mach, ip uint32, op stackvm.Op) {
 	ps, cs, err := m.Stacks()
 	if err != nil {
-		logf("...                    @0x%04x FAILED to get stacks: %v", ip, err)
+		logf("... % *v @0x%04x 0x%04x:0x%04x 0x%04x:0x%04x ERROR %v", opWidth, "", ip, m.PBP(), m.PSP(), m.CSP(), m.CBP(), err)
 	} else {
-		logf("...                    @0x%04x %v :0x%04x 0x%04x: %v", ip, ps, m.PSP(), m.CSP(), cs)
+		logf("... % *v @0x%04x %v :0x%04x 0x%04x: %v", opWidth, "", ip, ps, m.PSP(), m.CSP(), cs)
 	}
 }
 
