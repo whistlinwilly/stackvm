f7f5b307534f6e036f0fb1c34ed4c8cdbf995a3a
Joshua T Corbin <jcorbin@wunjo.org>
2017-03-30 08:44:41 -0700

    Expand the collatz sequence test

--- vm_test.go
+++ vm_test.go
@@ -1,6 +1,7 @@
 package stackvm_test
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/jcorbin/stackvm/x"
@@ -39,52 +40,65 @@ func TestMach_basic_math(t *testing.T) {
 }
 
 func TestMach_collatz_sequence(t *testing.T) {
-	TestCase{
-		StackSize: 64,
-		Prog: MustAssemble(
-			9, "push", "dup", // v v :
-			0x100, "push", // v v i :
-			"dup", 4, "add", "p2c", // v v i : i=i+4
-			"store", // v : i
+	tcs := make(TestCases, 0, 9)
+	for n := 1; n < 10; n++ {
+		vals := []uint32{uint32(n)}
+		val := vals[0]
+		for {
+			switch {
+			case val%2 == 0:
+				val = val / 2
+			default:
+				val = 3*val + 1
+			}
+			vals = append(vals, val)
+			if val <= 1 {
+				break
+			}
+		}
 
-			"loop:",         // v : i
-			"dup", 2, "mod", // v v%2 : ...
+		tcs = append(tcs, TestCase{
+			Name:      fmt.Sprintf("collatz(%d)", n),
+			StackSize: 64,
+			Prog: MustAssemble(
+				n, "push", "dup", // v v :
+				0x100, "push", // v v i :
+				"dup", 4, "add", "p2c", // v v i : i=i+4
+				"store", // v : i
 
-			":odd", "jnz",
+				"loop:",         // v : i
+				"dup", 2, "mod", // v v%2 : ...
 
-			"even:",
-			2, "div", // v/2 : ...
-			":next", "jump",
+				":odd", "jnz",
 
-			"odd:",
-			3, "mul", 1, "add", // 3*v+1 : ...
+				"even:",
+				2, "div", // v/2 : ...
+				":next", "jump",
 
-			"next:",
-			"dup",    // v v : i
-			"c2p",    // v v i :
-			"dup",    // v v i i :
-			4, "add", // v v i i+4 :
-			"p2c",   // v v i : i=i+4
-			"store", // v : i
-			"dup",   // v v : i
-			1, "eq", // v v==1 : i
-			":loop", "jz", // v : i
+				"odd:",
+				3, "mul", 1, "add", // 3*v+1 : ...
 
-			"c2p",         // v i :
-			0x100, "push", // v i base :
-			2, "p2c", // v : i base
-			"halt",
-		),
-		Result: Result{
-			Values: [][]uint32{{
-				9,
-				28, 14, 7,
-				22, 11,
-				34, 17,
-				52, 26, 13,
-				40, 20, 10, 5,
-				16, 8, 4, 2, 1,
-			}},
-		},
-	}.Run(t)
+				"next:",
+				"dup",    // v v : i
+				"c2p",    // v v i :
+				"dup",    // v v i i :
+				4, "add", // v v i i+4 :
+				"p2c",   // v v i : i=i+4
+				"store", // v : i
+				"dup",   // v v : i
+				1, "eq", // v v==1 : i
+				":loop", "jz", // v : i
+
+				"c2p",         // v i :
+				0x100, "push", // v i base :
+				2, "p2c", // v : i base
+				"halt",
+			),
+			Result: Result{
+				Values: [][]uint32{vals},
+			},
+		})
+	}
+
+	tcs.Run(t)
 }
