4a8db258c9432d0529cc04eb504a8643684249c7
Joshua T Corbin <jcorbin@wunjo.org>
2017-04-01 14:57:13 -0700

    Make TraceAction a flag.Getter

--- x/action/action.go
+++ x/action/action.go
@@ -1,6 +1,13 @@
 package action
 
-import "github.com/jcorbin/stackvm"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/jcorbin/stackvm"
+)
+
+var errInvalidTraceAction = errors.New("invalid trace action")
 
 // TraceAction represents one of the tracer methods.
 type TraceAction int
@@ -20,3 +27,42 @@ const (
 
 // Test returns true if the current trace action is the received one.
 func (ta TraceAction) Test(act TraceAction, _ uint32, _ stackvm.Op) bool { return act == ta }
+
+func (ta TraceAction) String() string {
+	switch ta {
+	case TraceBegin:
+		return "begin"
+	case TraceEnd:
+		return "end"
+	case TraceQueue:
+		return "queue"
+	case TraceBefore:
+		return "before"
+	case TraceAfter:
+		return "after"
+	default:
+		return fmt.Sprintf("InvalidTraceAction(%d)", int(ta))
+	}
+}
+
+// Get returns the trace action value for the flag.Getter interface.
+func (ta *TraceAction) Get() interface{} { return *ta }
+
+// Set sets the trace action value from a flag string.
+func (ta *TraceAction) Set(s string) error {
+	switch s {
+	case "begin":
+		*ta = TraceBegin
+	case "end":
+		*ta = TraceEnd
+	case "queue":
+		*ta = TraceQueue
+	case "before":
+		*ta = TraceBefore
+	case "after":
+		*ta = TraceAfter
+	default:
+		return errInvalidTraceAction
+	}
+	return nil
+}
