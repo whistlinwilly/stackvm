afccb7fd1630fd75432b626348af593302db8bae
Joshua T Corbin <jcorbin@wunjo.org>
2017-04-03 08:52:22 -0700

    Factor out log tracer

--- x/logf.go
+++ x/logf.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/jcorbin/stackvm"
-	"github.com/jcorbin/stackvm/internal/errors"
 	"github.com/jcorbin/stackvm/x/action"
 	"github.com/jcorbin/stackvm/x/dumper"
 	"github.com/jcorbin/stackvm/x/tracer"
@@ -15,104 +14,65 @@ func NewLogfTracer(f func(string, ...interface{})) stackvm.Tracer {
 	return tracer.Multi(
 		tracer.NewIDTracer(),
 		tracer.NewCountTracer(),
+		tracer.NewLogTracer(f),
 		&logfTracer{
-			f:   f,
 			dmw: action.Never,
 		},
 	)
 }
 
-const noteWidth = 15
-
 type logfTracer struct {
-	f   func(string, ...interface{})
 	dmw action.Predicate
 }
 
 func (lf *logfTracer) Context(m *stackvm.Mach, key string) (interface{}, bool) { return nil, false }
 
 func (lf *logfTracer) Begin(m *stackvm.Mach) {
-	lf.note(m, "===", "Begin", "stacks=[0x%04x:0x%04x]", m.PBP(), m.CBP())
 	if lf.dmw.Test(action.TraceBegin, 0, stackvm.Op{}) {
-		lf.dumpMem(m, "...")
+		dumpMem(m)
 	}
 }
 
 func (lf *logfTracer) End(m *stackvm.Mach) {
-	if err := m.Err(); err != nil {
-		lf.note(m, "===", "End", "err=%v", errors.Cause(err))
-	} else if vs, err := m.Values(); err != nil {
-		lf.note(m, "===", "End", "values_err=%v", err)
-	} else {
-		lf.note(m, "===", "End", "values=%v", vs)
-	}
 	if lf.dmw.Test(action.TraceEnd, 0, stackvm.Op{}) {
-		lf.dumpMem(m, "...")
+		dumpMem(m)
 	}
 }
 
 func (lf *logfTracer) Before(m *stackvm.Mach, ip uint32, op stackvm.Op) {
-	lf.noteStack(m, ">>>", op)
 	if lf.dmw.Test(action.TraceBefore, ip, op) {
-		lf.dumpMem(m, "...")
+		dumpMem(m)
 	}
 }
 
 func (lf *logfTracer) After(m *stackvm.Mach, ip uint32, op stackvm.Op) {
-	lf.noteStack(m, "...", "")
 	if lf.dmw.Test(action.TraceAfter, ip, op) {
-		lf.dumpMem(m, "...")
+		dumpMem(m)
 	}
 }
 
 func (lf *logfTracer) Queue(m, n *stackvm.Mach) {
-	mid, _ := m.Tracer().Context(m, "id")
-	lf.note(n, "+++", fmt.Sprintf("%v copy", mid))
 	if lf.dmw.Test(action.TraceQueue, 0, stackvm.Op{}) {
-		lf.dumpMem(m, "...")
+		dumpMem(m)
 	}
 }
 
 func (lf *logfTracer) Handle(m *stackvm.Mach, err error) {
-	if err != nil {
-		lf.note(m, "!!!", err)
+	if lf.dmw.Test(action.TraceHandle, 0, stackvm.Op{}) {
+		dumpMem(m)
 	}
 }
 
-func (lf *logfTracer) noteStack(m *stackvm.Mach, mark string, note interface{}) {
-	ps, cs, err := m.Stacks()
-	if err != nil {
-		lf.note(m, mark, note,
-			"0x%04x:0x%04x 0x%04x:0x%04x ERROR %v",
-			m.PBP(), m.PSP(), m.CSP(), m.CBP(), err)
-	} else {
-		lf.note(m, mark, note,
-			"%v :0x%04x 0x%04x: %v",
-			ps, m.PSP(), m.CSP(), cs)
-	}
+func defaultLogf(format string, args ...interface{}) {
+	fmt.Printf(format+"\n", args...)
 }
 
-func (lf *logfTracer) note(m *stackvm.Mach, mark string, note interface{}, args ...interface{}) {
-	mid, _ := m.Tracer().Context(m, "id")
-	count, _ := m.Tracer().Context(m, "count")
-	format := "%v #% 4d %s % *v @0x%04x"
-	parts := []interface{}{mid, count, mark, noteWidth, note, m.IP()}
-	if len(args) > 0 {
-		if s, ok := args[0].(string); ok {
-			format += " " + s
-			args = args[1:]
+func dumpMem(m *stackvm.Mach) {
+	logf := defaultLogf
+	if v, def := m.Tracer().Context(m, "logf"); def {
+		if f, ok := v.(func(string, ...interface{})); ok {
+			logf = f
 		}
-		parts = append(parts, args...)
 	}
-	lf.f(format, parts...)
-}
-
-func (lf *logfTracer) dumpMem(m *stackvm.Mach, mark string) {
-	mid, _ := m.Tracer().Context(m, "id")
-	pfx := []interface{}{mid, mark}
-	dumper.Dump(m, func(format string, args ...interface{}) {
-		format = "%v       %s " + format
-		args = append(pfx[:2:2], args...)
-		lf.f(format, args...)
-	})
+	dumper.Dump(m, logf)
 }
---
+++ x/tracer/log.go
@@ -0,0 +1,84 @@
+package tracer
+
+import (
+	"fmt"
+
+	"github.com/jcorbin/stackvm"
+	"github.com/jcorbin/stackvm/internal/errors"
+)
+
+const noteWidth = 15
+
+// NewLogTracer creates a tracer that logs machine state using a printf-style
+// string "logging" function
+func NewLogTracer(f func(string, ...interface{})) stackvm.Tracer {
+	return logfTracer(f)
+}
+
+type logfTracer func(string, ...interface{})
+
+func (lf logfTracer) Context(m *stackvm.Mach, key string) (interface{}, bool) {
+	if key != "logf" {
+		return nil, false
+	}
+	mid, _ := m.Tracer().Context(m, "id")
+	pfx := fmt.Sprintf("%v	   ... ", mid)
+	return func(format string, args ...interface{}) {
+		lf(pfx+format, args...)
+	}, true
+}
+
+func (lf logfTracer) Begin(m *stackvm.Mach) {
+	lf.note(m, "===", "Begin", "stacks=[0x%04x:0x%04x]", m.PBP(), m.CBP())
+}
+
+func (lf logfTracer) End(m *stackvm.Mach) {
+	if err := m.Err(); err != nil {
+		lf.note(m, "===", "End", "err=%v", errors.Cause(err))
+	} else if vs, err := m.Values(); err != nil {
+		lf.note(m, "===", "End", "values_err=%v", err)
+	} else {
+		lf.note(m, "===", "End", "values=%v", vs)
+	}
+}
+
+func (lf logfTracer) Queue(m, n *stackvm.Mach) {
+	mid, _ := m.Tracer().Context(m, "id")
+	lf.note(n, "+++", fmt.Sprintf("%v copy", mid))
+}
+
+func (lf logfTracer) Handle(m *stackvm.Mach, err error) {
+	if err != nil {
+		lf.note(m, "!!!", err)
+	}
+}
+
+func (lf logfTracer) Before(m *stackvm.Mach, ip uint32, op stackvm.Op) { lf.noteStack(m, ">>>", op) }
+func (lf logfTracer) After(m *stackvm.Mach, ip uint32, op stackvm.Op)  { lf.noteStack(m, "...", "") }
+func (lf logfTracer) noteStack(m *stackvm.Mach, mark string, note interface{}) {
+	ps, cs, err := m.Stacks()
+	if err != nil {
+		lf.note(m, mark, note,
+			"0x%04x:0x%04x 0x%04x:0x%04x ERROR %v",
+			m.PBP(), m.PSP(), m.CSP(), m.CBP(), err)
+	} else {
+		lf.note(m, mark, note,
+			"%v :0x%04x 0x%04x: %v",
+			ps, m.PSP(), m.CSP(), cs)
+	}
+}
+
+func (lf logfTracer) note(m *stackvm.Mach, mark string, note interface{}, args ...interface{}) {
+	mid, _ := m.Tracer().Context(m, "id")
+	count, _ := m.Tracer().Context(m, "count")
+	format := "%v #% 4d %s % *v @0x%04x"
+	parts := []interface{}{mid, count, mark, noteWidth, note, m.IP()}
+	if len(args) > 0 {
+		if s, ok := args[0].(string); ok {
+			format += " " + s
+			args = args[1:]
+		}
+		parts = append(parts, args...)
+	}
+	lf(format, parts...)
+}
