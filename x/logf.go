5f939b71c952deeeafebc77783cdbf4fd5917bf8
Joshua T Corbin <jcorbin@wunjo.org>
2017-04-03 18:14:44 -0700

    Use a Filtered FuncTracer for memory dumping

--- x/logf.go
+++ x/logf.go
@@ -15,52 +15,11 @@ func NewLogfTracer(f func(string, ...interface{})) stackvm.Tracer {
 		tracer.NewIDTracer(),
 		tracer.NewCountTracer(),
 		tracer.NewLogTracer(f),
-		&logfTracer{
-			dmw: action.Never,
-		},
+		tracer.Filtered(
+			tracer.FuncTracer(dumpMem),
+			action.Never,
+		),
 	)
-}
-
-type logfTracer struct {
-	dmw action.Predicate
-}
-
-func (lf *logfTracer) Context(m *stackvm.Mach, key string) (interface{}, bool) { return nil, false }
-
-func (lf *logfTracer) Begin(m *stackvm.Mach) {
-	if lf.dmw.Test(action.TraceBegin, 0, stackvm.Op{}) {
-		dumpMem(m)
-	}
-}
-
-func (lf *logfTracer) End(m *stackvm.Mach) {
-	if lf.dmw.Test(action.TraceEnd, 0, stackvm.Op{}) {
-		dumpMem(m)
-	}
-}
-
-func (lf *logfTracer) Before(m *stackvm.Mach, ip uint32, op stackvm.Op) {
-	if lf.dmw.Test(action.TraceBefore, ip, op) {
-		dumpMem(m)
-	}
-}
-
-func (lf *logfTracer) After(m *stackvm.Mach, ip uint32, op stackvm.Op) {
-	if lf.dmw.Test(action.TraceAfter, ip, op) {
-		dumpMem(m)
-	}
-}
-
-func (lf *logfTracer) Queue(m, n *stackvm.Mach) {
-	if lf.dmw.Test(action.TraceQueue, 0, stackvm.Op{}) {
-		dumpMem(m)
-	}
-}
-
-func (lf *logfTracer) Handle(m *stackvm.Mach, err error) {
-	if lf.dmw.Test(action.TraceHandle, 0, stackvm.Op{}) {
-		dumpMem(m)
-	}
 }
 
 func defaultLogf(format string, args ...interface{}) {
