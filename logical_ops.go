a6a85bd8a83907d2cf0066568ec2d0218d9ca15c
Joshua T Corbin <jcorbin@wunjo.org>
2017-04-07 07:50:00 -0700

    Consolidate logical ops

--- logical_ops.go
+++ logical_ops.go
@@ -97,6 +97,76 @@ func _gte(m *Mach) error {
 	return m.push(0)
 }
 
+func _not(m *Mach) error {
+	addr, err := m.pAddr(0)
+	if err != nil {
+		return err
+	}
+	i, _, pg := m.pageFor(addr)
+	if pg.d[i] == 0 {
+		pg.d[i] = 1
+	} else {
+		pg.d[i] = 0
+	}
+	return nil
+}
+
+func _and(m *Mach) error {
+	val, err := m.pop()
+	if err != nil {
+		return err
+	}
+	addr, err := m.pAddr(1)
+	if err != nil {
+		return err
+	}
+	i, _, pg := m.pageFor(addr)
+	if pg.d[i] != 0 && val != 0 {
+		pg.d[i] = 1
+	} else {
+		pg.d[i] = 0
+	}
+	return nil
+}
+
+func _or(m *Mach) error {
+	val, err := m.pop()
+	if err != nil {
+		return err
+	}
+	addr, err := m.pAddr(1)
+	if err != nil {
+		return err
+	}
+	i, _, pg := m.pageFor(addr)
+	if pg.d[i] != 0 || val != 0 {
+		pg.d[i] = 1
+	} else {
+		pg.d[i] = 0
+	}
+	return nil
+}
+
+func _xor(m *Mach) error {
+	val, err := m.pop()
+	if err != nil {
+		return err
+	}
+	addr, err := m.pAddr(1)
+	if err != nil {
+		return err
+	}
+	i, _, pg := m.pageFor(addr)
+	if pg.d[i] != 0 && val == 0 {
+		pg.d[i] = 1
+	} else if pg.d[i] == 0 && val != 0 {
+		pg.d[i] = 1
+	} else {
+		pg.d[i] = 0
+	}
+	return nil
+}
+
 func (arg _ltImm) run(m *Mach) error {
 	a, err := m.pop()
 	if err != nil {
@@ -203,4 +273,32 @@ func gte(arg uint32, have bool) op {
 		return _gte
 	}
 	return _gteImm(arg).run
+}
+
+func not(arg uint32, have bool) op {
+	if have {
+		return nil
+	}
+	return _not
+}
+
+func and(arg uint32, have bool) op {
+	if have {
+		return nil
+	}
+	return _and
+}
+
+func or(arg uint32, have bool) op {
+	if have {
+		return nil
+	}
+	return _or
+}
+
+func xor(arg uint32, have bool) op {
+	if have {
+		return nil
+	}
+	return _xor
 }
