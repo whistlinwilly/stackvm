8c89b3a22a1d3c482738cb6fcd02faca080bf56e
Joshua T Corbin <jcorbin@wunjo.org>
2017-04-07 07:59:50 -0700

    Add "SEND + MORE + MONEY" test

--- vm_test.go
+++ vm_test.go
@@ -7,6 +7,10 @@ import (
 	. "github.com/jcorbin/stackvm/x"
 )
 
+// TODO use:
+// - mark / loop et al
+// - mark / ret (fbret et al)
+
 func TestMach_basic_math(t *testing.T) {
 	TestCases{
 		{
@@ -143,5 +147,115 @@ func TestMach_collatz_explore(t *testing.T) {
 			{Values: [][]uint32{{2, 4, 1, 2, 4, 8}}},
 			{Values: [][]uint32{{2, 4, 1, 2, 4, 1}}},
 		}.WithExpectedHaltCodes(1),
+	}.Run(t)
+}
+
+func TestMach_send_more_money(t *testing.T) {
+	TestCase{
+		Name: "send more money (bottom up)",
+		Prog: MustAssemble(
+			0x40,
+
+			//     s e n d
+			// +   m o r e
+			// -----------
+			//   m o n e y
+
+			// used   [10]uint32 @0x0100    TODO use a bit vector
+			// values [8]uint32  @0x0140
+			//                   0 1 2 3 4 5 6 7
+			//                   d e y n r o s m
+
+			//// d + e = y  (mod 10)
+
+			0x0140, "cpush", 0x0140+4*8, "cpush", // : 0x0140 0x0160
+
+			0x0140+4*0, "push", ":choose", "call", // $d :
+			0x0140+4*1, "push", ":choose", "call", // $d $e :
+			"add", "dup", // $d+e $d+e :
+			10, "mod", // $d+e ($d+e)%10 :
+			"dup", 0x0140+4*2, "store", // $d+e $y :   -- $y=($d+e)%10
+			":markUsed", "call", // $d+e :
+			10, "div", // carry :
+
+			//// carry + n + r = e  (mod 10)
+
+			"dup",               // carry carry :
+			0x0140+4*1, "fetch", // carry carry $e :
+			"swap",                                // carry $e carry :
+			0x0140+4*3, "push", ":choose", "call", // carry $e carry $n :
+			"add", "sub", 10, "mod", // carry ($e-(carry+$n))%10 :
+			"dup", 0x0140+4*4, "store", // carry $r :   -- $r=($e-(carry+$n))%10
+			":markUsed", "call", // carry :
+			0x0140+4*3, "fetch", // carry $n :
+			0x0140+4*4, "fetch", // carry $n $r :
+			"add", "add", 10, "div", // carry :
+
+			//// carry + e + o = n  (mod 10)
+
+			"dup",               // carry carry :
+			0x0140+4*1, "fetch", // carry carry $e :
+			"add",               // carry carry+$e :
+			0x0140+4*3, "fetch", // carry carry+$e $n :
+			"swap", "sub", // carry $n-(carry+$e) :
+			10, "mod", // carry ($n-(carry+$e))%10 :
+			"dup", 0x0140+4*5, "store", // carry $o :   -- $o=($n-(carry+$e))%10
+			":markUsed", "call", // carry :
+			0x0140+4*1, "fetch", // carry $e :
+			0x0140+4*5, "fetch", // carry $e $o :
+			"add", "add", 10, "div", // carry :
+
+			//// carry + s + m = o  (mod 10)
+
+			"dup",                                 // carry carry :
+			0x0140+4*6, "push", ":choose", "call", // carry carry $s :
+			"add",               // carry carry+$s :
+			0x0140+4*5, "fetch", // carry carry+$s $o :
+			"swap", "sub", // carry $o-(carry+$s) :
+			10, "mod", // carry ($o-(carry+$s))%10 :
+			"dup", 0x0140+4*7, "store", // carry $m :   -- $m=($o-(carry+$s))%10
+			":markUsed", "call", // carry :
+			0x0140+4*6, "fetch", // carry $s :
+			"dup", 1, "hz", // carry $s :   -- guard $s != 0
+			0x0140+4*7, "fetch", // carry $s $m :
+			"dup", 1, "hz", // carry $s $m :   -- guard $m != 0
+			"add", "add", 10, "div", // carry :
+
+			//// carry = m  (mod 10)
+			0x0140+4*7, "fetch", // carry $m
+			"eq", 3, "hz",
+
+			//// Done
+			0, "halt",
+
+			"choose:",                        // &$X : retIp
+			0, "push", ":chooseLoop", "jump", // &$X i=0 : retIp
+			"chooseNext:", 1, "add", // &$X i++ : retIp
+			"chooseLoop:",                        // &$X i : retIp
+			"dup", 9, "lt", ":chooseNext", "fnz", // &$X i : retIp   -- fork next if i < 9
+			"dup", 2, "swap", "store", // $X=i: retIp
+			"dup", // $X $X : retIP   -- dup as arg for fallsthrough to markUsed
+
+			"markUsed:",                     // $X : retIp
+			4, "mul", 0x0100, "push", "add", // ... &used[$X]
+			"dup", "fetch", // ... &used[$X] used[$X]
+			2, "hnz", // ... &used[$X]
+			1, "push", "swap", "store", // ... -- used[$X] = 1
+			"ret", // :
+
+		),
+
+		Result: Results{
+			{Values: [][]uint32{{
+				7, // d
+				5, // e
+				2, // y
+				6, // n
+				8, // r
+				0, // o
+				9, // s
+				1, // m
+			}}},
+		}.WithExpectedHaltCodes(1, 2, 3),
 	}.Run(t)
 }
