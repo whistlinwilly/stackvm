930d2e8778f82c5c7d402676adccf2b186ef7a18
Joshua T Corbin <jcorbin@wunjo.org>
2017-04-29 11:39:38 -0700

    bench.sh: take extra arg

--- bench.sh
+++ bench.sh
@@ -4,5 +4,8 @@ set -e
 name=$1
 branch=$(basename "$(git symbolic-ref HEAD)")
 out="prof.$name.$branch"
+if [ -n "$2" ]; then
+    out+=".$2"
+fi
 
 go test . -v -run "$name" -bench "$name" -benchmem -cpuprofile "$out.cpu" -memprofile "$out.mem" | tee "$out.bench"
