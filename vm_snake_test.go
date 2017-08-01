c3bb12586bab6415f98c9ffcd2b7238e63351a49
Joshua T Corbin <jcorbin@wunjo.org>
2017-08-01 07:48:29 -0700

    snake: drop commented out prototype

--- vm_snake_test.go
+++ vm_snake_test.go
@@ -200,70 +200,8 @@ var snakeSolTest = TestCase{
 	// Result: XXX,
 }
 
-func genSnakeCube(rng uint32, m int) []bool {
-	n := m * m * m
-	limit := n * n
-
-reboot:
-	// fmt.Printf("reboot!\n")
-	freedoms := make([]bool, 0, n)
-	oc := make([]bool, n)
-	var lastPos, lastDir [3]int
-
-	oc[0] = true
-	freedoms = append(freedoms, true)
-
-	i := 0
-	c := 0
-	for {
-		if c++; c > limit {
-			goto reboot
-		}
-		rng = lcgM*rng + lcgC
-
-		pos := lastPos
-		delta := 1
-		if rng%2 == 1 {
-			delta = -1
-		}
-		pos[rng%3] += delta
-
-		if pos[0] < 0 || pos[0] >= m || pos[1] < 0 || pos[1] >= m || pos[2] < 0 || pos[2] >= m {
-			continue
-		}
-
-		j := m*m*pos[0] + m*pos[1] + pos[2]
-		if !oc[j] {
-			// fmt.Printf("bop [%v] %v\n", i, pos)
-			// TODO: trace these choices to build the snake
-
-			dir := [3]int{pos[0] - lastPos[0], pos[1] - lastPos[1], pos[2] - lastPos[2]}
-			if lastDir == [3]int{0, 0, 0} {
-				freedoms = append(freedoms, false)
-			} else if dir == lastDir {
-				freedoms = append(freedoms, false)
-			} else {
-				freedoms = append(freedoms, true)
-			}
-
-			lastDir, lastPos, oc[j] = dir, pos, true
-			if i++; i >= n-1 {
-				break
-			}
-		}
-	}
-
-	// fmt.Printf("done!\n")
-
-	if cap(freedoms) != len(freedoms) {
-		panic("genSnakeCube failed to generate a complete snake cube")
-	}
-
-	return freedoms
-}
-
-
 */
+
 type fastRNG struct{ state *uint32 }
 
 func makeFastRNG(seed uint32) fastRNG { return fastRNG{state: &seed} }
