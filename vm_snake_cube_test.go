4dc7c54d9593dd2222640a5e3693d10276913539
Bill Westlin <bwestlin@uber.com>
2017-08-09 01:53:10 -0700

    Generate starting cubes

--- vm_snake_cube_test.go
+++ vm_snake_cube_test.go
@@ -20,12 +20,9 @@ import (
 // (serving double duty during solving) set back to the starting position.
 
 // Generating starting moves:
-// The trickery here is to avoid tedium in generation. It seems to me the
-// best solution is to start with the cube array containing 1 in all starting
-// positions, then sweep through this array and when we find 1 (fork to sweep)
-// set current position to this index before clearing the remaining 1 bits.
-// This leaves us with as many machines as we had set bits, where each is
-// ready to begin the standard generate(-fork)-check loop.
+// Originally I had considered some generative approach to build starting machines
+// but most straightforward is a function that just pushes one of the few starting
+// positions on the stack and forks to the generate function
 
 // Generating next moves:
 // This part is easy, if we have a fixed position in the snake (and not start)
@@ -45,36 +42,78 @@ import (
 // If these checks pass, we made a valid move into an unoccupied cell, time to
 // generate more.
 
-func TestMach_snake_cube(t *testing.T) {
-	TestCases{
-		{
-			Name: "Snake cube solver",
-			Err:  "HALT(1)",
-			Prog: MustAssemble(
-				// Sample Snake 1 Ascii Art!
-				// |X|X|X|
-				//     |X|X|
-				//       |X|
-				//       |X|X|
-				//         |X|
-				//         |X|X|
-				//           |X|X|X|
-				//               |X|
-				//               |X|X|
-				//                 |X|X|
-				//                   |X|
-				//                   |X|X|X|
-				//                       |X|
-				//                       |X|X|X|
+const CUBE_MEM = 0x100
 
-				// String encoding "fffaaafaafaaafafaaaafafafaf"
-				// Bool encoding 000111011011101011110101010
-				0x40,
-				1, "halt",
-			),
-			Result: Result{
-				Err: "HALT(1)",
-			},
-		},
-	}.Run(t)
+var snakeTest = TestCase{
+	Name: "Snake cube solver",
+	Prog: MustAssemble(
+		// Sample Snake 1 Ascii Art!
+		// |X|X|X|
+		//     |X|X|
+		//       |X|
+		//       |X|X|
+		//         |X|
+		//         |X|X|
+		//           |X|X|X|
+		//               |X|
+		//               |X|X|
+		//                 |X|X|
+		//                   |X|
+		//                   |X|X|X|
+		//                       |X|
+		//                       |X|X|X|
+
+		// String encoding "fffaaafaafaaafafaaaafafafaf"
+		// Bool encoding 000111011011101011110101010
+
+		// Problem / Solution memory is defined over 0x100-0x26c
+		//
+		// Cube memory 					0x100 - 0x16c [27]uint32	CUBE_MEM
+		// Starting position 		0x174 				uint32 			START_POS
+		// Snake memory 				0x200 - 0x26c [27]uint32	SNAKE_MEM
+
+		0x40, // stack size
+
+		CUBE_MEM, "cpush", CUBE_MEM+4*(27+1), "cpush", // : 0x0100 0x0174 -- for returning the solution (including starting position)
+		":generate_starting_cubes", "call",
+		":solve", "call",
+		0, "halt",
+
+		// defined functions
+		"generate_starting_cubes:", // : retIp
+		// starts at (0,0,0)
+		0, "push", ":starting_position_set", "fork", // : retIp -- child has 0 on parameter stack
+		// starts at (1,0,0)
+		1, "push", ":starting_position_set", "fork", // : retIp -- child has 1 on parameter stack
+		// starts at (1, 1, 0)
+		4, "push", ":starting_position_set", "fork", // : retIp -- child has 4 on parameter stack
+		// starts at (1, 1, 1)
+		13, "push", ":starting_position_set", "fork", // : retIp -- child has 13 on parameter stack
+		"starting_position_set:",
+		"ret",
+
+		"solve:",
+		"ret",
+	),
+
+	Result: Results{
+		{Values: generateEmptyValues()},
+		{Values: generateEmptyValues()},
+		{Values: generateEmptyValues()},
+		{Values: generateEmptyValues()},
+		{Values: generateEmptyValues()},
+	},
 }
+
+func generateEmptyValues() [][]uint32 {
+	var empty [][]uint32
+	var subEmpty []uint32
+	for i := 0; i < 28; i++ {
+		subEmpty = append(subEmpty, 0)
+	}
+	empty = append(empty, subEmpty)
+	return empty
+}
+
+func TestMach_snake_cube(t *testing.T)      { snakeTest.Run(t) }
+func BenchmarkMach_snake_cube(b *testing.B) { snakeTest.Bench(b) }
